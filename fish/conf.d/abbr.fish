60bdf43ec2a66b2433bd17ca6389f436b5258a19
Ivan Frolov <iv.frolov@cian.ru>
2021-08-10 14:13:38 +0300

    Adds git pull fish abbreviation

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -4,5 +4,6 @@ if status is-interactive
     abbr --add --global gcm 'git commit -m'
     abbr --add --global gco 'git checkout'
     abbr --add --global gpu 'git push --set-upstream'
+    abbr --add --global gpf 'git pull --ff-only'
     abbr --add --global gs 'git status'
 end
