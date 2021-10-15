a2c71cfcf0441e5ce3be6de83e5482271b96a02b
Ivan Frolov <iv.frolov@cian.ru>
2021-10-15 09:35:42 +0300

    Adds git branch switching abbreviations

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -2,8 +2,10 @@ if status is-interactive
     abbr --add --global dc 'docker compose'
 
     abbr --add --global gcm 'git commit -m'
-    abbr --add --global gco 'git checkout'
-    abbr --add --global gpu 'git push --set-upstream'
+    abbr --add --global gpu 'git push --set-upstream origin (git branch --show-current)'
+    abbr --add --global gsm 'git switch (git_default_branch)'
+    abbr --add --global gsc 'git switch -c'
     abbr --add --global gpf 'git pull --ff-only'
+    abbr --add --global gp 'git push'
     abbr --add --global gs 'git status'
 end
