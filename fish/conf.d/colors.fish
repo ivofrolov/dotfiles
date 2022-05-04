285f28a6f3e45864cce58e9922c6173003728a9f
Ivan Frolov <iv.frolov@cian.ru>
2022-05-04 16:50:46 +0300

    Adjusts kitty terminal colors

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -2,23 +2,23 @@ set -g fish_color_command blue
 set -g fish_color_quote green
 set -g fish_color_redirection blue
 set -g fish_color_end magenta
-set -g fish_color_error --dim red
+set -g fish_color_error red
 set -g fish_color_param blue
 set -g fish_color_comment brblack
 set -g fish_color_selection --background=brwhite
 set -g fish_color_operator blue
 set -g fish_color_escape magenta
 set -g fish_color_autosuggestion white
-set -g fish_color_cwd --dim cyan
-set -g fish_color_user --dim green
+set -g fish_color_cwd cyan
+set -g fish_color_user green
 set -g fish_color_host normal
-set -g fish_color_host_remote --dim yellow
-set -g fish_color_status --dim red
+set -g fish_color_host_remote yellow
+set -g fish_color_status red
 set -g fish_color_cancel brblack
 set -g fish_color_search_match --background=brwhite
 
-set -g fish_pager_color_progress brblack --bold
-set -g fish_pager_color_prefix normal --bold
+set -g fish_pager_color_progress --bold brblack
+set -g fish_pager_color_prefix --bold normal
 set -g fish_pager_color_completion normal
 set -g fish_pager_color_description yellow
 set -g fish_pager_color_selected_background --background=brwhite
