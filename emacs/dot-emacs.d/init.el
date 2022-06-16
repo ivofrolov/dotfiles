bd7222135d1677de89a1860aa71b8ac88d256650
Ivan Frolov <ivofrolov@fastmail.com>
2022-06-16 19:53:11 +0300

    Moves customized emacs options to init file

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -1,24 +1,68 @@
+(set-default 'inhibit-startup-screen t)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(set-frame-font "Hack 13" nil t)
+
+(setq modus-themes-syntax '(green-strings)
+      modus-themes-subtle-line-numbers t)
+(load-theme 'modus-operandi)
+
+(set-default 'mode-line-position-column-format '(" C%C"))
+(set-default 'mode-line-position-column-line-format '(" (%l,%C)"))
+(global-display-line-numbers-mode)
+(column-number-mode)
+
+(set-default 'cursor-type 'bar)
+(blink-cursor-mode 0)
+
+(set-default 'indent-tabs-mode nil)
+(set-default 'standard-indent 4)
+(set-default 'tab-width 4)
+
+(set-default 'global-visual-line-mode t)
+(set-default 'visual-line-fringe-indicators '(nil right-curly-arrow))
+
+(global-set-key (kbd "C-S-k") #'kill-whole-line)
+(global-set-key (kbd "s-c") #'clipboard-kill-ring-save)
+(global-set-key (kbd "s-x") #'clipboard-kill-region)
+(global-set-key (kbd "s-v") #'clipboard-yank)
+(set-default 'select-enable-clipboard nil)
+(set-default 'select-enable-primary t)
+
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+(set-default 'create-lockfiles nil)
+(set-default 'make-backup-files nil)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(create-lockfiles nil)
- '(cursor-type 'bar)
- '(custom-enabled-themes '(modus-operandi))
- '(inhibit-startup-screen t)
- '(make-backup-files nil)
- '(package-selected-packages '(lua-mode))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :family "Hack")))))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
+(use-package dash-at-point
+  :ensure t
+  :bind (("C-c d" . dash-at-point)
+         ("C-c e" . dash-at-point-with-docset)))
+
+(use-package elm-mode
+  :ensure t)
+
+(use-package lua-mode
+  :ensure t)
+
