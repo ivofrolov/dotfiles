0293fc21450841712a4a83e460f6e52657f23920
Ivan Frolov <ivofrolov@fastmail.com>
2022-07-19 09:00:06 +0300

    Adds emacs auto package updates

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -94,5 +94,11 @@
 (use-package lua-mode
   :ensure t)
 
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (auto-package-update-maybe))
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
