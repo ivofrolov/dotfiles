7123b7df3550d07cf3837caf1d48781184a76ab5
Ivan Frolov <iv.frolov@cian.ru>
2024-03-19 15:16:31 +0300

    Makes Kitty edit file hint more accurate

--- kitty/dot-config/kitty/custom-find-file-hints.py
+++ kitty/dot-config/kitty/custom-find-file-hints.py
@@ -9,21 +9,29 @@ def _make_paths_regexp(paths):
 
 
 def mark(text, args, Mark, extra_cli_args, *a):
-    pattern = _make_paths_regexp(path.name for path in Path.cwd().iterdir())
+    pattern = _make_paths_regexp(
+        path.name + ("/" if path.is_dir() else "")
+        for path in Path.cwd().iterdir()
+    )
     idx = 0
     for match in re.finditer(pattern, text):
         groupdict = {}
-        path = re.sub(r"[\r\n\0]", "", match.group(0))
-        if linecol_match := re.search(r"\:(\d+)(?:\:(\d+))?$", path):
+        start = match.start()
+        end = match.end()
+        path = match.group(0)
+        if linecol_match := re.search(r"\:(\d+)(?:\:(\d+))?", path):
             groupdict["line"] = linecol_match.group(1)
             groupdict["col"] = linecol_match.group(2)
             path = path[:linecol_match.start()]
-        if func_matches := list(re.finditer(r"\:\:(\w+)", path)):
+            end = start + linecol_match.end()
+        elif func_matches := list(re.finditer(r"\:\:(\w+)", path)):
             groupdict["func"] = ".".join(m.group(1) for m in func_matches)
             path = path[:func_matches[0].start()]
-        if Path(path).is_file():
-            idx += 1
-            yield Mark(idx, match.start(), match.end(), path, groupdict)
+            end = start + func_matches[-1].end()
+        # TODO: check that Path(path).is_file() for each path without last part after line end
+        idx += 1
+        path = re.sub(r"[\r\n\0]", "", path)
+        yield Mark(idx, start, end, path, groupdict)
 
 
 def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
