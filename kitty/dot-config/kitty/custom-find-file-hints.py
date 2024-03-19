7d5b82a920d09690028bf70cf22de18128acf26e
Ivan Frolov <iv.frolov@cian.ru>
2024-03-19 09:54:19 +0300

    Makes Kitty edit file hint more accurate

--- kitty/dot-config/kitty/custom-find-file-hints.py
+++ kitty/dot-config/kitty/custom-find-file-hints.py
@@ -3,41 +3,38 @@ import re
 import shlex
 
 
-LINE_REGEXP = r"\:(\d+)"
-FUNC_REGEXP = r"\:\:([^\s\:]+)"
-
-
-def make_paths_regexp(paths):
+def _make_paths_regexp(paths):
     query = "|".join(re.escape(path) for path in paths)
     return fr"(?:{query})[\S\r\n]*"
 
 
-def re_extract(pattern, string):
-    return re.sub(pattern, "", string), re.findall(pattern, string)
-
-
-def clean_path(string):
-    return string.strip(":")
-
-
 def mark(text, args, Mark, extra_cli_args, *a):
-    pattern = make_paths_regexp(path.name for path in Path.cwd().iterdir())
-    for idx, match in enumerate(re.finditer(pattern, text)):
-        start, end = match.span()
-        mark_text = re.sub(r"[\r\n\0]", "", text[start:end])
-        yield Mark(idx, start, end, mark_text, {})
+    pattern = _make_paths_regexp(path.name for path in Path.cwd().iterdir())
+    idx = 0
+    for match in re.finditer(pattern, text):
+        groupdict = {}
+        path = re.sub(r"[\r\n\0]", "", match.group(0))
+        if linecol_match := re.search(r"\:(\d+)(?:\:(\d+))?$", path):
+            groupdict["line"] = linecol_match.group(1)
+            groupdict["col"] = linecol_match.group(2)
+            path = path[:linecol_match.start()]
+        if func_matches := list(re.finditer(r"\:\:(\w+)", path)):
+            groupdict["func"] = ".".join(m.group(1) for m in func_matches)
+            path = path[:func_matches[0].start()]
+        if Path(path).is_file():
+            idx += 1
+            yield Mark(idx, match.start(), match.end(), path, groupdict)
 
 
 def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
-    for path in data["match"]:
-        path = clean_path(path)
-        path, line_col = re_extract(LINE_REGEXP, path)
-        path, func = re_extract(FUNC_REGEXP, path)
-        cmd = ["emacsclient", "-n"]
-        if line_col:
-            cmd.append("+" + ":".join(line_col[:2]))
-        elif func:
-            pass  # TODO: cmd.append(f"-e (find-file-function {path} {func})")
+    for path, context in zip(data["match"], data["groupdicts"]):
+        cmd = ["edit"]
+        if line := context.get("line"):
+            linecol = f"+{line}"
+            if col := context.get("col"):
+                linecol += f":{col}"
+            cmd.append(linecol)
+        # TODO: cmd.append(f"-e (find-file-function {path} {func})")
         cmd.append(path)
         text = " ".join(shlex.quote(arg) for arg in cmd)
         window = boss.window_id_map.get(target_window_id)
