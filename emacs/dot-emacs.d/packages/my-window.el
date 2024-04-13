479cfe5845ec292914225d160c306f072116236c
Ivan Frolov <ivofrolov@fastmail.com>
2024-04-13 08:47:04 +0300

    Adjusts Emacs configuration

--- emacs/dot-emacs.d/packages/my-window.el
+++ emacs/dot-emacs.d/packages/my-window.el
@@ -1,10 +1,12 @@
 (require 'window)
 
-(defun my-move-buffer-other-window ()
-  "Move the current buffer to the other window."
-  (interactive)
+(defun my-move-buffer-other-window (arg)
+  "Move current buffer to the other window.
+With \\[universal-argument] prefix stay in selected window."
+  (interactive "P")
   (let ((buffer (current-buffer)))
     (previous-buffer)
-    (switch-to-buffer-other-window buffer)))
+    (switch-to-buffer-other-window buffer)
+    (if arg (select-window (previous-window)))))
 
 (provide 'my-window)
