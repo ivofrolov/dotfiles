4ae5f7dcc5d8178f6b1447b79ef3ac5556a05d3e
Ivan Frolov <ivofrolov@fastmail.com>
2024-11-22 22:10:10 +0300

    Adjusts MacOS defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -8,3 +8,6 @@ defaults -currentHost write -globalDomain AppleFontSmoothing -int 0
 # Dock
 defaults -currentHost write com.apple.dock autohide -bool true
 defaults -currentHost write com.apple.dock mineffect -string scale
+
+# Enable Touch ID for sudo
+sed "s/^#auth/auth/" /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local
