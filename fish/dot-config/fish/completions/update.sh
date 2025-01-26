d40f75730fb0323dd34bf6414c7640e52b46108d
Ivan Frolov <ivofrolov@fastmail.com>
2025-01-26 09:14:33 +0300

    Update docker completions

---
+++ fish/dot-config/fish/completions/update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -Ls -o docker.fish https://raw.githubusercontent.com/dldevinc/fish-docker/main/completions/docker.fish
+curl -Ls -o docker-compose.fish https://raw.githubusercontent.com/dldevinc/fish-docker/main/completions/docker-compose.fish
