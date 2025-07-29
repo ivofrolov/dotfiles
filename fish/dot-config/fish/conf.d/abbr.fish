793a594d6242694c2fcbe9f15be1b08704fdfe6a
Ivan Frolov <ivofrolov@yandex.ru>
2025-07-29 11:12:09 +0300

    Adjust fish shell configuration

--- fish/dot-config/fish/conf.d/abbr.fish
+++ fish/dot-config/fish/conf.d/abbr.fish
@@ -2,7 +2,6 @@ if status is-interactive
     abbr --add --global dc 'docker compose'
 
     abbr --add --global gcm 'git commit -m'
-    abbr --add --global gpu 'git push --set-upstream origin (git branch --show-current)'
     abbr --add --global gsm 'git switch (git_default_branch)'
     abbr --add --global gsc 'git switch -c'
     abbr --add --global gpf 'git pull --ff-only'
