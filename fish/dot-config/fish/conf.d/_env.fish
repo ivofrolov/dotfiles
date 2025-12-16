5779c81f3053d30965fd19d075e45a2ac17357a1
Ivan Frolov <ivofrolov@fastmail.com>
2025-12-16 08:34:38 +0300

    Adjusts Fish configuration

--- fish/dot-config/fish/conf.d/_env.fish
+++ fish/dot-config/fish/conf.d/_env.fish
@@ -1,12 +1,7 @@
 set -g __fish_git_prompt_show_informative_status 'yes'
 set fish_greeting
 
-set --global --export HOMEBREW_PREFIX "/opt/homebrew";
-set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
-fish_add_path --global --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
-if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
-if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
+/opt/homebrew/bin/brew shellenv | source
 
 set -x EDITOR nano
 set -x PAGER less
