cce4b446130d97db24333397deae9de0a98ecdb4
Ivan Frolov <ivofrolov@fastmail.com>
2025-12-30 09:14:23 +0300

    Adjusts Fish configuration

--- fish/dot-config/fish/conf.d/colors.fish
+++ fish/dot-config/fish/conf.d/colors.fish
@@ -1,26 +1,26 @@
 # https://fishshell.com/docs/current/interactive.html#syntax-highlighting
 
-set -g fish_color_command black
-set -g fish_color_quote blue
-set -g fish_color_redirection black
-set -g fish_color_end black
-set -g fish_color_error red
-set -g fish_color_param black
-set -g fish_color_comment brblack
-set -g fish_color_selection --background=brwhite
-set -g fish_color_operator black
-set -g fish_color_escape magenta
-set -g fish_color_autosuggestion white
-set -g fish_color_cwd cyan
-set -g fish_color_user green
-set -g fish_color_host normal
-set -g fish_color_host_remote yellow
-set -g fish_color_status red
-set -g fish_color_cancel brblack
-set -g fish_color_search_match --background=brwhite
+set fish_color_command black
+set fish_color_quote blue
+set fish_color_redirection black
+set fish_color_end black
+set fish_color_error red
+set fish_color_param black
+set fish_color_comment brblack
+set fish_color_selection --background=brwhite
+set fish_color_operator black
+set fish_color_escape magenta
+set fish_color_autosuggestion white
+set fish_color_cwd cyan
+set fish_color_user green
+set fish_color_host normal
+set fish_color_host_remote yellow
+set fish_color_status red
+set fish_color_cancel brblack
+set fish_color_search_match --background=brwhite
 
-set -g fish_pager_color_progress --bold brblack
-set -g fish_pager_color_prefix --bold normal
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description yellow
-set -g fish_pager_color_selected_background --background=white
+set fish_pager_color_progress --bold brblack
+set fish_pager_color_prefix --bold normal
+set fish_pager_color_completion normal
+set fish_pager_color_description yellow
+set fish_pager_color_selected_background --background=white
