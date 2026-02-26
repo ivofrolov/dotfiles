89e72167b2831eb0a06fe95f4f3f328e3f7d3d0a
Ivan Frolov <ivofrolov@yandex.ru>
2026-02-26 08:31:14 +0300

    Adjust fish shell configuration

--- fish/dot-config/fish/conf.d/_env.fish
+++ fish/dot-config/fish/conf.d/_env.fish
@@ -1,14 +1,12 @@
 set -g __fish_git_prompt_show_informative_status 'yes'
 set fish_greeting
 
-/opt/homebrew/bin/brew shellenv | source
+command -q brew; or /opt/homebrew/bin/brew shellenv | source
 
 set -x EDITOR nano
 set -x PAGER less
 set -x LESS '-iRF'
 
 if status is-interactive
-    if command -s direnv > /dev/null 2>&1
-        direnv hook fish | source
-    end
+    command -q direnv; and direnv hook fish | source
 end
--- fish/dot-config/fish/conf.d/alias.fish
+++ fish/dot-config/fish/conf.d/alias.fish
@@ -2,13 +2,8 @@ if status is-interactive
     alias urlencode "string escape --style=url"
     alias urldecode "string unescape --style=url"
 
-    if command -s bat > /dev/null 2>&1
-        alias cat bat
-    end
-
-    if command -s trash > /dev/null 2>&1
-        alias rm trash
-    end
+    command -q bat; and alias cat bat
+    command -q trash; and alias rm trash
 
     if string match -eq 'kitty' -- $TERM
         alias ssh "kitten ssh"
